0eb86075b414095c18d7f8ac0634534b98af2804
Feb-in1 <febinfrancisk10@gmail.com>
2024-06-13 13:31:51 +0530

    Sign in updated

--- app.js
+++ app.js
@@ -11,17 +11,51 @@ mongoose.connect("mongodb+srv://feb:febin4475@cluster0.pydodfi.mongodb.net/BlogD
 
 const generateHashedPassword = async (password) => {
     const salt = await bcrypt.genSalt(10)
-    return bcrypt.hash(password,salt)
+    return bcrypt.hash(password, salt)
 }
-app.post("/signup", async(req, res) => {
+
+//api for signup
+app.post("/signup", async (req, res) => {
     let input = req.body
-    let hashedPassword=await generateHashedPassword(input.pass)
+    let hashedPassword = await generateHashedPassword(input.pass)
     console.log(hashedPassword)
-    input.pass=hashedPassword       //hashed passwd is snd to the db
-    let blog= new blogmodel(input)
+    input.pass = hashedPassword       //hashed passwd is snd to the db
+    let blog = new blogmodel(input)
     blog.save()
     res.json({ "status": "success" })
 })
+
+//api for SignIn
+
+app.post("/signin", (req, res) => {
+    let input = req.body
+    blogmodel.find({ "em": req.body.em }).then(                    //"em" : req.body.em  both em must be same 
+        (response) => {
+            //if the email given is wrong , an EMPTY ARRAY will be displayed, else the data will be shown
+
+            // console.log(response)                           
+            if (response.length > 0) {
+                let dbpass = response[0].pass
+                console.log(dbpass)
+                bcrypt.compare(input.pass,dbpass,(error,ismatch)=>{
+                    if (ismatch) {
+                        res.json({"status":"success","userid":response[0]._id})
+                        
+                    } else {
+                        res.json({"status":"Incorrect Password"})
+                    }
+                })
+            } else {
+                res.json({ "status": "User Not Found!!!" })
+
+            }
+
+        }
+    )
+    // res.json({ "status": "success" })
+})
+
+
 
 app.listen(8080, () => {
     console.log("Server Started!")
